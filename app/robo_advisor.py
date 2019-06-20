85d873525afa7d47a0267b27c49faf0afee3f3a7
jsr512 <50751736+jsr512@users.noreply.github.com>
2019-06-20 19:36:12 -0400

    Get latest high price

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -27,6 +27,14 @@ latest_day = dates[0]
 
 latest_close = tsd[latest_day]["4. close"]
 
+high_prices = []
+
+for target_list in dates:
+    high_price = tsd[latest_day]["2. high"]
+    high_prices.append(float(high_price))
+
+recent_high = max(high_prices) # max of all high
+
 
 #Information Output
 
@@ -38,11 +46,11 @@ print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print("RECENT HIGH: $101,000.00")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
 print("HAPPY INVESTING!")
-print("-------------------------")
+print("-------------------------") 
